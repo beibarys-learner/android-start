ce3835016b1f0e9ddc5e64056745274883084f4a
javamentor2022 <javamentor2022@gmail.com>
2024-02-04 01:37:08 +0600

    update layout_width 200dp

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
     private lateinit var tvTipPercentLabel: TextView;
     private lateinit var tvTipAmount: TextView;
     private lateinit var tvTotalAmount: TextView;
+    private lateinit var tvTipDescription: TextView;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +28,8 @@ class MainActivity : ComponentActivity() {
         tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel);
         tvTipAmount = findViewById(R.id.tvTipAmount);
         tvTotalAmount = findViewById(R.id.tvTotalAmount);
+        tvTipDescription = findViewById(R.id.tvTipDescription)
+
 
         seekBarTip.progress = INITIAL_TIP_PERCENT
         tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
@@ -36,6 +39,7 @@ class MainActivity : ComponentActivity() {
                 Log.i(TAG, "onProgressChanged $progress")
                 tvTipPercentLabel.text = "$progress%"
                 computeTipTotal()
+                updateTipDescription(progress)
 100
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -53,6 +57,18 @@ class MainActivity : ComponentActivity() {
                 computeTipTotal()
             }
         })
+    }
+
+    private fun updateTipDescription(tipPercent: Int) {
+        val tipDescription = when (tipPercent){
+            in 0..9 -> "Poor"
+            in 10..14 -> "Acceptable"
+            in 15..19 -> "Good"
+            in 20..25 -> "Excellent"
+            else -> "Amazing"
+        }
+        tvTipDescription.text = tipDescription
+
     }
 
     private fun computeTipTotal(){
